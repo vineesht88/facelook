50373a76e37c83b5760b7d718e32ea2eca775be8
Vineesh Thankachan <vineeshthankachandp@gmail.com>
2025-10-15 18:43:11 +0400

    slider bug

--- src/layout/carousel.tsx
+++ src/layout/carousel.tsx
@@ -1,4 +1,4 @@
-import React, { type JSX } from "react";
+import type { JSX } from "react";
 import brand01 from "../assets/images/brands/Client-01.png";
 import brand02 from "../assets/images/brands/Client-02.png";
 import brand03 from "../assets/images/brands/Client-03.png";
@@ -6,7 +6,7 @@ import brand04 from "../assets/images/brands/Client-04.png";
 import brand05 from "../assets/images/brands/Client-05.png";
 
 const images = [brand01, brand02, brand03, brand04, brand05];
-// Duplicate for seamless infinite loop
+// Duplicate for seamless looping
 const loopImages = [...images, ...images];
 
 export default function ImageCarousel(): JSX.Element {
